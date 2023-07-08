59732c04eb862e216b8e47c841139c448064cb3b
Luke <lhazlebeck@gmail.com>
2023-07-08 03:31:10 -0400

    calculator

--- js-code.js
+++ js-code.js
@@ -1,3 +1,30 @@
+let displayNum = document.querySelector('#display');
+displayNum.textContent = '69';
+
+let num1 = 0;
+let num2 = 0;
+let currentOp = null;
+
+let CE = document.querySelector("#clear").addEventListener("click",function(){
+    displayNum.innerHTML = '0';
+    num1 = '0';
+    num2 = 0;
+    currentOp = null;
+});
+
+let numbers = document.querySelectorAll(".num");
+for (let i = 0; i < numbers.length; i++){
+    numbers[i].addEventListener("click", function(){
+        if (displayNum.textContent === '0'){
+            displayNum.textContent = numbers[i].id;
+        }
+        else {
+            displayNum.textContent += numbers[i].id;
+        }
+    });
+}
+
+
 function add(a, b){
     return parseInt(a)+parseInt(b);
 }
@@ -31,15 +58,15 @@ function operator(op, num1, num2){
     return "Give me a valid operation, damnit.";
 }
 
-let num1 = 3;
-let num2 = 5;
-console.log(add(num1, num2) + "\n" + subtract(num1, num2) + "\n" + multiply(num1,num2) + "\n" + divide(multiply(num1, num1), num1));
+//let num1 = 3;
+//let num2 = 5;
+//console.log(add(num1, num2) + "\n" + subtract(num1, num2) + "\n" + multiply(num1,num2) + "\n" + divide(multiply(num1, num1), num1));
 
-const oper = prompt("What operation do you want to do?");
-num1 = prompt("Number 1");
-num2 = prompt("Number 2");
-const calculation = operator(oper, num1, num2);
+//const oper = prompt("What operation do you want to do?");
+//num1 = prompt("Number 1");
+//num2 = prompt("Number 2");
+//const calculation = operator(oper, num1, num2);
 
-const answer = document.createElement('h1');
-answer.textContent = calculation;
-document.querySelector('body').appendChild(answer);
+//const answer = document.createElement('h1');
+//answer.textContent = calculation;
+//document.querySelector('body').appendChild(answer);
